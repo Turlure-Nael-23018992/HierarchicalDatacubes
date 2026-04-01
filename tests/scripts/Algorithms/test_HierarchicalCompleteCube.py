fbd193807c7ba9c44054df91f703e80458053dda
Turlure-Nael-23018992 <146089336+Turlure-Nael-23018992@users.noreply.github.com>
2026-04-01 14:15:58 +0200

    moved file for tests

--- tests/scripts/Algorithms/test_HierarchicalCompleteCube.py
+++ tests/scripts/Algorithms/test_HierarchicalCompleteCube.py
@@ -15,7 +15,7 @@ from collections import defaultdict
 from itertools import product as iterproduct
 
 # Add project root to sys.path
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
